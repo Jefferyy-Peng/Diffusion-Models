fix bugs in ddpm, noise calculation, pbar

DDPM.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import os
 
 import torch
@@ -41,23 +42,23 @@ class ForwardDiffusion:
         model.eval()
         with torch.no_grad():
             z = torch.randn(n, 3, self.image_size, self.image_size).to(self.device)
-            for i in tqdm(range(reversed(self.noise_steps))):
+            for i in tqdm(reversed(range(self.noise_steps)), position=0):
                 t = (torch.ones(n) * i).long().to(self.device)
-                predicted_noise = model(x, t)
+                predicted_noise = model(z, t)
                 alpha = self.alpha[t][:, None, None, None]
                 alpha_hat = self.alpha_hat[t][:, None, None, None]
                 beta = self.beta[t][:, None, None, None]
                 if i > 1:
-                    noise = torch.randn_like(x)
+                    noise = torch.randn_like(z)
                 else:
-                    noise = torch.zeros_like(x)
-                x = 1 / torch.sqrt(alpha) * (
-                            x - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise) + torch.sqrt(
+                    noise = torch.zeros_like(z)
+                z = 1 / torch.sqrt(alpha) * (
+                            z - ((1 - alpha) / (torch.sqrt(1 - alpha_hat))) * predicted_noise) + torch.sqrt(
                     beta) * noise
-            model.train()
-            x = (x.clamp(-1, 1) + 1) / 2
-            x = (x * 255).type(torch.uint8)
-            return x
+        model.train()
+        z = (z.clamp(-1, 1) + 1) / 2
+        z = (z * 255).type(torch.uint8)
+        return z
 
 def get_data(args):
     transforms = torchvision.transforms.Compose([
@@ -78,15 +79,17 @@ def train(args):
     dataloader = get_data(args)
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-    pbar = tqdm(dataloader)
     logger = SummaryWriter(os.path.join("runs", args.run_name))
+    l = len(dataloader)
     for epoch in range(args.epochs):
-        for i, (image, _) in enumerate(dataloader):
+        logging.info(f"Starting epoch {epoch}:")
+        pbar = tqdm(dataloader)
+        for i, (image, _) in enumerate(pbar):
             image = image.to(device)
             t = diffusion.sample_timestamps(image.shape[0])
-            x_t, noise_pred = diffusion.noise_image(image, t)
+            x_t, noise = diffusion.noise_image(image, t)
             noise_pred = model(x_t, t)
-            loss = criterion(noise_pred, noise_pred)
+            loss = criterion(noise, noise_pred)
 
             optimizer.zero_grad()
             loss.backward()
@@ -95,9 +98,14 @@ def train(args):
             pbar.set_postfix(MSE=loss.item())
             logger.add_scalar("MSE", loss.item(), global_step=epoch * l + i)
 
-        sampled_images = diffusion.sample(model, n=image.shape[0])
+        sampled_images = diffusion.sample(model, n=dataloader.batch_size)
+        if not os.path.exists(os.path.join("results", args.run_name)):
+            os.makedirs(os.path.join("results", args.run_name))
         save_images(sampled_images, os.path.join("results", args.run_name, f"{epoch}.jpg"))
+        if not os.path.exists(os.path.join("models", args.run_name)):
+            os.makedirs(os.path.join("models", args.run_name))
         torch.save(model.state_dict(), os.path.join("models", args.run_name, f"ckpt.pt"))
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()

modules.py
@@ -76,12 +76,12 @@ class SelfAttention(nn.Module):
         )
 
     def forward(self, x):
-        x = x.view(-1, self.channels, self.size * self.size).swapaxes(1, 2)
-        x_ln = self.ln(x)
-        attention_value, _ = self.mha(x_ln, x_ln, x_ln)
+        x = x.view(-1, self.emb_dim, self.size * self.size).swapaxes(1, 2)
+        x_norm = self.norm(x)
+        attention_value, _ = self.mha(x_norm, x_norm, x_norm)
         attention_value = attention_value + x
-        attention_value = self.ff_self(attention_value) + attention_value
-        return attention_value.swapaxes(2, 1).view(-1, self.channels, self.size, self.size)
+        attention_value = self.ff(attention_value) + attention_value
+        return attention_value.swapaxes(2, 1).view(-1, self.emb_dim, self.size, self.size)
 
 
 class UNet(nn.Module):
@@ -121,25 +121,25 @@ class UNet(nn.Module):
 
     def forward(self, x, t):
         t = t.unsqueeze(-1).type(torch.float)  # t is timestamp
-        t = self.pos_encoding(t, self.time_dim)
+        pos_emb = self.pos_encoding(t, self.time_dim)
 
         x1 = self.inc(x)
-        x2 = self.down1(x1, t)
+        x2 = self.down1(x1, pos_emb)
         x2 = self.sa1(x2)
-        x3 = self.down2(x2, t)
+        x3 = self.down2(x2, pos_emb)
         x3 = self.sa2(x3)
-        x4 = self.down3(x3, t)
+        x4 = self.down3(x3, pos_emb)
         x4 = self.sa3(x4)
 
         x4 = self.bot1(x4)
         x4 = self.bot2(x4)
         x4 = self.bot3(x4)
 
-        x = self.up1(x4, x3, t)
+        x = self.up1(x4, x3, pos_emb)
         x = self.sa4(x)
-        x = self.up2(x, x2, t)
+        x = self.up2(x, x2, pos_emb)
         x = self.sa5(x)
-        x = self.up3(x, x1, t)
+        x = self.up3(x, x1, pos_emb)
         x = self.sa6(x)
         output = self.outc(x)
         return output